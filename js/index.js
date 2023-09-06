9e972c64359dadd8c4894750951e0d68a4446a51
Nikita Suslov <130926726+NikitaSuslov@users.noreply.github.com>
2023-09-06 17:38:58 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ search.addEventListener('click', () =>{
                     break;
 
                 case 'Clouds':
-                    image.src = 'img/cloud.png';
+                    image.src = '/img/cloud.png';
                     break;
 
                 case 'Haze':
